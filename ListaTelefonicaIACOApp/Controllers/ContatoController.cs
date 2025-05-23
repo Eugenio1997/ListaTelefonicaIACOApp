c3f200e7f2330e681f2fea159bd923f355118a75
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-05-23 16:46:57 -0300

    [WIP] Adicionando logica de paginacao

--- ListaTelefonicaIACOApp/Controllers/ContatoController.cs
+++ ListaTelefonicaIACOApp/Controllers/ContatoController.cs
@@ -1,8 +1,11 @@
-﻿using ListaTelefonicaIACOApp.Models;
+﻿using Dapper;
+using ListaTelefonicaIACOApp.Infrastructure;
+using ListaTelefonicaIACOApp.Models;
 using ListaTelefonicaIACOApp.ViewModels;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Oracle.ManagedDataAccess.Client;
+using NuGet.Protocol.Plugins;
+using System.Data;
+
 
 namespace ListaTelefonicaIACOApp.Controllers
 {
@@ -10,91 +13,57 @@ namespace ListaTelefonicaIACOApp.Controllers
     {
         private readonly ILogger<ContatoController> _logger;
         private readonly IConfiguration? _configuration;
+        private readonly ListaTelefonicaDbContext _context;
+        public ContatoViewModel contato = new ContatoViewModel();
         int registrosPorPagina = 10;
-        int TotalRegistros;
-        int TotalPaginas;
+        int totalRegistros;
+        int totalPaginas;
         int offset;
-        public ContatoController(ILogger<ContatoController> logger, IConfiguration configuration)
+        private IDbConnection _conn;
+
+        public ContatoController(ILogger<ContatoController> logger,
+                                 IConfiguration configuration,
+                                 ListaTelefonicaDbContext context
+                                   )
         {
             _logger = logger;
             _configuration = configuration;
+            _context = context;
         }
 
-        private ContatoViewModel ObterColaboradoresPaginados(int pagina, int registrosPorPagina, out int totalPaginas)
+        private async Task<List<ContatoViewModel>> ObterColaboradoresPaginados(int registrosPorPagina = 10, int paginaAtual = 1)
         {
-            int offset = (pagina - 1) * registrosPorPagina;
-            int totalRegistros = 0;
-            totalPaginas = 0;
+            offset = (paginaAtual - 1) * registrosPorPagina;
+            string ordenacao = "NOME"; // default order by NOME
+            string query = @$"SELECT *
+                        FROM LISTA_FONES 
+                        ORDER BY {ordenacao}
+                        OFFSET {offset} ROWS
+                        FETCH NEXT {registrosPorPagina} ROWS ONLY";
 
-            var lista = new ContatoViewModel();
-            var connectionString = _configuration?.GetConnectionString("ListaTelefonicaIACOConnectionString");
-
-            using (var conn = new OracleConnection(connectionString))
-            {
-                conn.Open();
-
-                using (var countCmd = conn.CreateCommand())
-                {
-                    countCmd.CommandText = "SELECT COUNT(*) FROM LISTA_FONES";
-                    totalRegistros = Convert.ToInt32(countCmd.ExecuteScalar());
-                }
-
-                totalPaginas = (int)Math.Ceiling((double)totalRegistros / registrosPorPagina);
-
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = $@"
-                        SELECT *
-                        FROM LISTA_FONES
-                        ORDER BY ID
-                        OFFSET {offset} ROWS FETCH NEXT {registrosPorPagina} ROWS ONLY";
-
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            lista.Colaboradores.Add(new ContatoViewModel
-                            {
-                                Id = reader.GetInt32(0),
-                                Nome = reader.GetString(1),
-                                Fixo = reader.GetString(2),
-                                Celular = reader.GetString(3),
-                                Comercial = reader.GetString(4),
-                                Endereco = reader.GetString(5),
-                                Email = reader.GetString(6)
-                            });
-                        }
-                    }
-                }
-            }
-
-            return lista;
+            //contatos paginados
+            List<ContatoViewModel> contatos = (await _conn.
+                                                    QueryAsync<ContatoViewModel>(query))
+                                                    .ToList();
+            return contatos;
         }
 
         // GET: ColaboradorController
-        public ActionResult Index(int paginaAtual = 1)
+        public async Task<IActionResult> Index(int paginaAtual = 1)
         {
-            int registrosPorPagina = 10;
-            var lista = ObterColaboradoresPaginados(paginaAtual, registrosPorPagina, out int totalPaginas);
+
+            _conn = _context.GetOpenConnection();
+            var totalRegistros = await _conn.QuerySingleAsync<int>("SELECT COUNT(*) FROM LISTA_FONES");
+            totalPaginas = (int)Math.Ceiling((double)totalRegistros / registrosPorPagina);
+
+            contato.Colaboradores = await ObterColaboradoresPaginados(registrosPorPagina, paginaAtual);
 
             ViewBag.PaginaAtual = paginaAtual;
             ViewBag.TotalPaginas = totalPaginas;
 
-            return View(lista);
+            return View(contato);
         }
 
-        public IActionResult PaginaDados(int paginaAtual = 1)
-        {
-
-
-            int registrosPorPagina = 10;
-            var lista = ObterColaboradoresPaginados(paginaAtual, registrosPorPagina, out int totalPaginas);
-
-            ViewBag.PaginaAtual = paginaAtual;
-            ViewBag.TotalPaginas = totalPaginas;
-
-            return PartialView("_TabelaColaboradores", lista);
-        }
 
         // GET: ColaboradorController/Details/5
         public ActionResult Details(int id)
@@ -125,7 +94,7 @@ namespace ListaTelefonicaIACOApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult FiltrarPorContatos(IFormCollection collection)
+        public async Task<ActionResult> FiltrarPorContatos(IFormCollection collection)
         {
             try
             {
@@ -138,7 +107,7 @@ namespace ListaTelefonicaIACOApp.Controllers
         }
 
         // GET: ColaboradorController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
             return View();
         }
@@ -146,7 +115,7 @@ namespace ListaTelefonicaIACOApp.Controllers
         // POST: ColaboradorController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, IFormCollection collection)
         {
             try
             {
@@ -159,7 +128,7 @@ namespace ListaTelefonicaIACOApp.Controllers
         }
 
         // GET: ColaboradorController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
             return View();
         }
@@ -167,7 +136,7 @@ namespace ListaTelefonicaIACOApp.Controllers
         // POST: ColaboradorController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
             try
             {
