ca85388040fcdaff6ef1bbc7ca47a805689fc820
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-07-10 02:38:52 -0300

    fix: removendo mascaras JQuery na tela de edicao

--- ListaTelefonicaIACOApp/ViewModels/Contato/ContatoEditViewModel.cs
+++ ListaTelefonicaIACOApp/ViewModels/Contato/ContatoEditViewModel.cs
@@ -9,21 +9,21 @@ namespace ListaTelefonicaIACOApp.ViewModels.Contato
 
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         [RegularExpression(@"^(?=.{3,})(?!.*\d)[A-Za-zÀ-ÖØ-öø-ÿ\s]+$", ErrorMessage = "O Nome deve ter no mínimo 3 letras e não pode conter números.")]
-        public string Nome { get; set; } = string.Empty;
-        [RegularExpression(@"^\(\d{2}\) \d{4}-\d{4}$", ErrorMessage = "Formato esperado: 99 9999-9999")]
+        public string Nome { get; set; }
+        //[RegularExpression(@"^\(\d{2}\) \d{4}-\d{4}$", ErrorMessage = "Formato esperado: 99 9999-9999")]
         public string? Fixo { get; set; }
 
-        [RegularExpression(@"^\(\d{2}\) \d{5}-\d{4}$", ErrorMessage = "Celular deve estar no formato 99 99999-9999")]
+        //[RegularExpression(@"^\(\d{2}\) \d{5}-\d{4}$", ErrorMessage = "Celular deve estar no formato 99 99999-9999")]
         public string? Celular { get; set; }
         [DisplayName("Ramal")]
-        [RegularExpression(@"^\d{4}$", ErrorMessage = "Formato esperado: 0000")]
+        //[RegularExpression(@"^\d{4}$", ErrorMessage = "Formato esperado: 0000")]
         public string? Comercial { get; set; }
 
         [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
         public string? Email { get; set; }
         [Required(ErrorMessage = "O endereço é Obrigatório.")]
         [Display(Name = "Endereço")]
-        public string Endereco { get; set; } = string.Empty;
+        public string Endereco { get; set; }
         public List<ContatoIndexViewModel> Contatos { get; set; } = new();
         [Display(Name = "Criado Em")]
         public string CriadoAs { get; set; } = string.Empty;
